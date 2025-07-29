949e9381edea1a790b83a5fe30ac105cc38bf502
Prithu Anan <prithua00@gmail.com>
2025-07-29 22:16:38 +0600

    added remote backend url

--- src/utils/api/api-config.ts
+++ src/utils/api/api-config.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'http://135.235.169.115:8080';
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://135.235.169.115:8080';
+// export const API_BASE_URL = 'http://localhost:8080';
 
 export const getAuthHeaders = () => {
     const token = localStorage.getItem("teamsync_jwt");
